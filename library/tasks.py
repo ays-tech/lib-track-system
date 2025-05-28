13227a4c244b87836e7842d55a797223a565d256
rabiu <medrabiu@gmail.com>
2025-05-28 14:06:01 +0100

    DEV

--- library/tasks.py
+++ library/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from .models import Loan
 from django.core.mail import send_mail
 from django.conf import settings
+import datetime
 
 @shared_task
 def send_loan_notification(loan_id):
@@ -18,3 +19,5 @@ def send_loan_notification(loan_id):
         )
     except Loan.DoesNotExist:
         pass
+
+d
